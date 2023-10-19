27bd01d848a15c53305a9fd1ba3b17552394b655
Markberyl <musangaberyl@gmail.com>
2023-10-19 09:49:36 +0000

    sql advanced

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,10 +1,6 @@
-SELECT
-    band_name,
-    CASE
-        WHEN formed = 0 THEN 0
-        ELSE IF(splits = 0, 2022 - formed, splits - formed)
-    END AS lifespan
-FROM metal_bands
-WHERE style = 'Glam rock'
-ORDER BY lifespan DESC;
-
+-- Lists all bands with Glam rock as their main style, ranked by their longevity.
+-- SELECT band_name, (IFNULL(split, YEAR(CURRENT_DATE())) - formed) AS lifespan
+SELECT band_name, (IFNULL(split, '2020') - formed) AS lifespan
+    FROM metal_bands
+    WHERE FIND_IN_SET('Glam rock', IFNULL(style, "")) > 0
+    ORDER BY lifespan DESC;
--- 0x00-MySQL_Advanced/6-bonus.sql
+++ 0x00-MySQL_Advanced/6-bonus.sql
@@ -1,25 +1,25 @@
-DELIMITER //
-
-CREATE PROCEDURE AddBonus(IN user_id INT, IN project_name VARCHAR(255), IN score INT)
+-- Creates a stored procedure AddBonus that adds a new
+-- correction for a student.
+DROP PROCEDURE IF EXISTS AddBonus;
+DELIMITER $$
+CREATE PROCEDURE AddBonus (user_id INT, project_name VARCHAR(255), score FLOAT)
 BEGIN
-    DECLARE project_id INT;
+    DECLARE project_count INT DEFAULT 0;
+    DECLARE project_id INT DEFAULT 0;
 
-    -- Check if the project already exists, if not, create it
-    SELECT id INTO project_id FROM projects WHERE name = project_name;
-
-    IF project_id IS NULL THEN
-        INSERT INTO projects (name) VALUES (project_name);
-        SET project_id = LAST_INSERT_ID();
+    SELECT COUNT(id)
+        INTO project_count
+        FROM projects
+        WHERE name = project_name;
+    IF project_count = 0 THEN
+        INSERT INTO projects(name)
+            VALUES(project_name);
     END IF;
-
-    -- Insert the correction
-    INSERT INTO corrections (user_id, project_id, score) VALUES (user_id, project_id, score);
-    
-    -- Update the user's average score
-    UPDATE users u
-    SET average_score = (SELECT AVG(score) FROM corrections WHERE user_id = u.id)
-    WHERE u.id = user_id;
-END //
-
+    SELECT id
+        INTO project_id
+        FROM projects
+        WHERE name = project_name;
+    INSERT INTO corrections(user_id, project_id, score)
+        VALUES (user_id, project_id, score);
+END $$
 DELIMITER ;
-
